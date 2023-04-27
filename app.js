7a91f0490249be4723af856ffcc52f2d339a44d1
Abdullah Al-Mahfadi <66219679+ab-almahfadi@users.noreply.github.com>
2023-04-27 23:33:59 +0800

    L9

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb+srv://admin-obad:11357900aa..@atlascluster.70y6xtb.mongodb.net/todolistDB');
+  await mongoose.connect('mongodb+srv://admin-obad:11357900aa..@atlascluster.70y6xtb.mongodb.net/todolistDB?retryWrites=true&w=majority');
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
@@ -184,9 +184,7 @@ app.get("/about", function(req, res){
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
-} else{
-  app.listen(port);
-}
+} 
 
 app.listen(port, function() {
   console.log("Server started on port seccessfully");
