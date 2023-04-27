3569a63b0394d37c1ef386fe5c1395c5d599850d
Abdullah Al-Mahfadi <66219679+ab-almahfadi@users.noreply.github.com>
2023-04-27 21:44:03 +0800

    Add gitignore and another change

--- app.js
+++ app.js
@@ -180,6 +180,12 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
+
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
