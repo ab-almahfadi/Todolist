d46dc91d9991b0474da02fe62a855c9bca4b0e67
Abdullah Al-Mahfadi <66219679+ab-almahfadi@users.noreply.github.com>
2023-04-27 23:17:49 +0800

    L8

--- app.js
+++ app.js
@@ -183,10 +183,10 @@ app.get("/about", function(req, res){
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 8000;
-} 
+  port = 3000;
+} else{
   app.listen(port);
-
+}
 
 app.listen(port, function() {
   console.log("Server started on port seccessfully");
