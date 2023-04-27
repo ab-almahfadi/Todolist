1c0ff529eb5d09461616ad93c5df31927a68e523
Abdullah Al-Mahfadi <66219679+ab-almahfadi@users.noreply.github.com>
2023-04-27 23:56:49 +0800

    L9

--- app.js
+++ app.js
@@ -181,12 +181,12 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-} 
+// let port = process.env.PORT;
+// if (port == null || port == "") {
+//   port = 3000;
+// } 
 
-app.listen(port, function() {
+app.listen(process.env.PORT, function() {
   console.log("Server started on port seccessfully");
 });
 
